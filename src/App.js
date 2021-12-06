6e387249cdc3e1006f41b92a8a0d20ccd67804e4
marnicodes <marni.ali.codes@gmail.com>
2021-12-06 15:49:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./ma-logo.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} id="App-logo" alt="logo" className="img-fluid" />
+      </header>
+    </div>
+  );
+}
