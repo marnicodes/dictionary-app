ef838c4d795e65268223b8d597ccf89a31953053
marnicodes <marni.ali.codes@gmail.com>
2021-12-07 13:15:08 +0100

    Created form in new component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./ma-logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <header className="App-header">
         <img src={logo} id="App-logo" alt="logo" className="img-fluid" />
       </header>
+      <Dictionary />
     </div>
   );
 }
