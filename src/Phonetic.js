524298821730c6d4f459507fc9a3c31358565e36
marnicodes <marni.ali.codes@gmail.com>
2021-12-16 21:16:10 +0100

    Updated Phonetic audio

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,18 +1,15 @@
-import ReactAudioPlayer from "react-audio-player";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
     <section className="Phonetic">
-      <div className="row">
-        <div className="col">
-          <ReactAudioPlayer src={props.phonetic.audio} onPlay controls />
-        </div>
-        <div className="col">
-          <h1>[{props.phonetic.text}]</h1>
-        </div>
-      </div>
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        {" "}
+        Listen
+      </a>
+
+      <div id="text">[{props.phonetic.text}]</div>
     </section>
   );
 }
