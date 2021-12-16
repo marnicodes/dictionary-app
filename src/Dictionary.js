5fc67a53855f3debb6af073ef957e539a623c405
marnicodes <marni.ali.codes@gmail.com>
2021-12-16 21:15:43 +0100

    Created default keyword state and basic styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,9 +4,11 @@ import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
-  let [keyword, setKeyword] = useState("props.defaultKeyword");
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
-  let [loaded, setLoaded] = useState(false); //keep track of component loading state as by default it is not loading. Hence, "false".
+  let [loaded, setLoaded] = useState(false);
+  //keep track of component loading state as
+  //by default it is not loading. Hence, "false".
 
   function handleResponse(response) {
     setResults(response.data[0]);
@@ -28,16 +30,22 @@ export default function Dictionary(props) {
   }
 
   function load() {
-    setLoaded(true);
-    search();
+    setLoaded(true); //to show loaded code below
+    search(); //but at the same search for the keyword (in state)
   }
 
   if (loaded) {
     return (
       <div className="Dictionary">
         <section>
+          <h1 id="heading">What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
-            <input type="search" autoFocus onChange={handleKeywordChange} />
+            <input
+              type="search"
+              autoFocus
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
           <div className="hint">suggested words: field, sunrise, jazz...</div>
         </section>
@@ -45,7 +53,7 @@ export default function Dictionary(props) {
       </div>
     );
   } else {
-    load();
+    load(); //if it is NOT loading, the function load will set it to "true" (look at load function)
     return "Loading";
   }
 }
