aed69143001ce42da9a6562d6618a200d446d876
marnicodes <marni.ali.codes@gmail.com>
2021-12-19 11:53:38 +0100

    Updated CSS section element

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,13 +3,13 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
-    <section className="Phonetic">
+    <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         {" "}
         Listen
       </a>
 
       <div id="text">[{props.phonetic.text}]</div>
-    </section>
+    </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -9,14 +9,15 @@ export default function Results(props) {
       <div className="Results">
         <section>
           <h1>{props.results.word}</h1>
+
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
         </section>
-        {props.results.phonetics.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetic phonetic={phonetic} />
-            </div>
-          );
-        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             // to return definition: return meaning.definitions[0].definition
